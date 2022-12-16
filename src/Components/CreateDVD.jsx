301a27dfa5f749dca43d23eff10787dd6e5a1a8f
yareenm <yareenm@outlook.com>
2022-12-16 13:33:04 -0600

    Updated CreateDVD.jsx

--- src/Components/CreateDVD.jsx
+++ src/Components/CreateDVD.jsx
@@ -31,25 +31,27 @@ function CreateDVD() {
 
   return (
     <>
-      <nav className="md:w-3/4 m-auto mt-8">
+    
+      <h1 className="w-full bg-green-600 text-gray-100 text-left py-4 text-4xl uppercase">
+      <nav className="md:w-6/4 m-auto mt-8" style ={{display:"inline", marginRight:"55px"}}>
         <button
           aria-label="Navigates back to the home page"
           onClick={() => navigate("/")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="48px"
+            height="36px"
             viewBox="0 0 24 24"
-            width="48px"
-            fill="#000000"
+            width="36px"
+            fill="#ffffff"
+            style={{position:"absolute", top:"18px",left:"5px"}}
           >
             <path d="M0 0h24v24H0V0z" fill="none" />
             <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
           </svg>
         </button>
       </nav>
-      <h1 className="w-full bg-green-600 text-gray-100 text-center py-4 text-4xl uppercase tracking-wide mt-20 md:w-1/2 mx-auto rounded-t-md">
-        Add New DVD
+        Create DVD
       </h1>
       <form
         className="grid grid-cols-2 md:w-1/2 mx-auto shadow-2xl rounded-md p-8"
@@ -118,13 +120,16 @@ function CreateDVD() {
           >
             Rating:
           </label>
-          <input
-            type="text"
-            id="rating"
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          <select id="rating"
+            name="rating"
             value={rating}
-            onChange={(e) => setRating(e.target.value)}
-          />
+            onChange={(e) => setRating(e.target.value)}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
         </div>
         {/* Notes */}
         <div className="w-full px-3 col-span-full mb-6 md:mb-4">
@@ -137,16 +142,24 @@ function CreateDVD() {
           <input
             type="text"
             id="notes"
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-full h-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             value={notes}
             onChange={(e) => setNotes(e.target.value)}
           />
         </div>
+        <button type="submit" 
+          onClick={() => navigate("/")} 
+          className="col-span-1 text-lg border-2 text-gray-100 border-green-600 bg-green-600 hover:bg-gray-50 hover:border-green-600  hover:text-green-600 hover:shadow-2xl  lg:w-4/5  rounded-full py-3 px-4 shadow-lg"
+          style={{marginTop:"40px",marginLeft:"20px"}}
+        >
+          Cancel
+        </button>
         <button
           type="submit"
-          className="col-span-full text-lg border-2 text-gray-100 border-green-600 bg-green-600 hover:bg-gray-50 hover:border-green-600  hover:text-green-600 hover:shadow-2xl  lg:w-2/5  justify-self-center rounded-full py-3 px-4 shadow-lg"
+          className="col-span-1 text-lg border-2 text-gray-100 border-green-600 bg-green-600 hover:bg-gray-50 hover:border-green-600  hover:text-green-600 hover:shadow-2xl  lg:w-4/5  rounded-full py-3 px-4 shadow-lg"
+          style={{marginTop:"40px",marginLeft:"20px"}}
         >
-          Add DVD
+          Create DVD
         </button>
       </form>
     </>
